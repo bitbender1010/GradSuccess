32f279b405180fbf302514ffee8b17921d5b636b
davidubanyi <ubanyi@gmail.com>
2019-03-12 14:32:05 +0100

    header is complete

--- src/components/header.js
+++ src/components/header.js
@@ -6,42 +6,37 @@ const Header = ({ siteTitle }) => (
   <header>
     <div>
       <Link to="/">
-          <div style={{
-        width: '150px',
-        margin: '.8em auto'
-        }}><Image /></div>
+        <div
+          style={{
+            width: "150px",
+            margin: ".5em auto .1em",
+          }}
+        >
+          <Image />
+        </div>
       </Link>
-      <ul style={{
-        display: 'flex',
-        listStyle: 'none',
-        justifyContent: 'space-around',
-        padding: '0 8em',
-        textTransform: 'uppercase',
-        background: '#4A4A4A'
-      }}>
-        <Link>
-          Admissions
-        </Link>
-        <li>
-          Job/Careers
-        </li>
-        <li>
-          Scholarships
-        </li>
-        <li>
-          About Us
-        </li>
-        <li>
-          Blog
-        </li>
-        <li>
-          Contact
-        </li>
+      <ul
+        style={{
+          display: "flex",
+          listStyle: "none",
+          justifyContent: "space-around",
+          padding: ".3em 8em",
+          textTransform: "uppercase",
+          background: "#4A4A4A",
+          color: "white",
+          fontWeight: "700",
+          fontSize: ".9em",
+        }}
+      >
+        <Link to="/Admissions">Admissions</Link>
+        <Link to="/Careers">Job/Careers</Link>
+        <Link to="/Scholarships">Scholarships</Link>
+        <Link to="About">About Us</Link>
+        <Link to="Blog">Blog</Link>
+        <Link to="Contact">Contact</Link>
       </ul>
     </div>
   </header>
 )
-
-
 
 export default Header
