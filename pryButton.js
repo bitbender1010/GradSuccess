f250a5df3288ca8f707182b46a7ff9786c19d36b
Ololade Dammie <37069209+bitbender1010@users.noreply.github.com>
2019-03-12 19:21:33 +0100

    Created PryButton file

---
+++ pryButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+const PryButton = () => (
+<div>
+    {/* */}
+    <button  style={{
+        color: '#47DCBC',
+        textAlign: 'center',
+        minWidth: '300px',
+        minHeight: '50px',
+        fontSize: '25px',
+        backgroundColor: 'yellow',
+        border: 'none',
+        margin: '0px auto'
+    }}> Get the help you need </button>
+</div>
+);
+
+export default PryButton 
