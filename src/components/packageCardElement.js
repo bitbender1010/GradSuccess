196e140292c349a49c3f603e5e133caaed6223e8
davidubanyi <ubanyi@gmail.com>
2019-03-14 09:10:39 +0100

    changed the config file and added the icon

--- src/components/packageCardElement.js
+++ src/components/packageCardElement.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react"
+import PryButton from "./pryButton"
 
-const PackageCardElement = ({imgUrl, title, text}) => (
-    <div>
-        <img src="" alt=""/>
-        <h2></h2>
+const PackageCardElement = ({ imgUrl, title, text }) => (
+  <div
+    stlye={{
+      width: "305px",
+    }}
+  >
+    <img src={imgUrl} alt="image for package" />
+    <div style={{
+        color: 'white'
+    }}>
+      <h2>{title}</h2>
+      <p>{text}</p>
+      <PryButton small={true} text="Get Started" />
     </div>
+  </div>
 )
 
 export default PackageCardElement
--- src/components/packageCards.js
+++ src/components/packageCards.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import PackageCardElement from './packageCardElement';
 
 
 const PackageCards = () => (
     <div>
-        
+        <PackageCardElement imgUrl title text />
+        <PackageCardElement imgUrl title text />
+        <PackageCardElement imgUrl title text />
     </div>
 )
 
